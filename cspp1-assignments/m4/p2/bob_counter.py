d0c1a5c8050fdb6168c993afccfd26296b9b6b15
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-02 19:32:08 +0530

    " -> 5 of 5 passed. pylint: 3.33/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -6,12 +6,12 @@ For example, if s = 'azcbobobegghakl', then your program should print
 Number of times bob occurs is: 2'''
 
 def main():
-	''' this program is used to print the bob count in a given string'''
-	STR = input()
+    ''' this program is used to print the bob count in a given string'''
+    STR = input()
     COUNT = 0
     for I in range(len(STR) - 2):
-    	if STR[I] == 'b' and STR[I+1] == 'o' and STR[I+2] == 'b':
-        	COUNT += 1
-	print(COUNT)
+        if STR[I] == 'b' and STR[I+1] == 'o' and STR[I+2] == 'b':
+            COUNT += 1
+    print(COUNT)
 if __name__== "__main__":
-	main()
+    main()
