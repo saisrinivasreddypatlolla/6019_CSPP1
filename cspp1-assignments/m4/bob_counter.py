89385e864c71b528b390c654b8d145a05d24ff1c
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-02 18:53:46 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- cspp1-assignments/m4/bob_counter.py
+++
@@ -1,7 +0,0 @@
-"""This program is used to print the bob counter"""
-STR = input("enter string ")
-COUNT = 0
-for I in range(len(STR) - 2):
-    if STR[I] == 'b' and STR[I+1] == 'o' and STR[I+2] == 'b':
-        COUNT += 1
-print(COUNT)
--- cspp1-assignments/m4/vowels_counter.py
+++
@@ -1,7 +0,0 @@
-"""this program is used to print number of vowels occured in a given string"""
-STR = input("enter string ")
-COUNT = 0
-for CHAR in STR:
-    if CHAR in ('a', 'e', 'i', 'o', 'u'):
-        COUNT += 1
-print(COUNT)
