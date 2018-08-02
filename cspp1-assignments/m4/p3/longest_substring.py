4fc914ae9e0c224c68beb9649df3dd32aa0ddc1f
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-02 19:57:40 +0530

    " -> 0 of 5 passed. pylint: 3.12/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -15,20 +15,20 @@ If you've spent more than a few hours on this problem, we suggest that you move 
 If you have time, come back to this problem after you've had a break and cleared your head.'''
 
 def main():
-	"""This program is used to print longest alphabetical sequence to the given string"""
-	STR = input("enter string ")
-	S = 0
-	COUNT1 = 0
-	COUNT = 0
-	for i in range(len(STR)-1):
-    	if STR[i] <= STR[i+1]:
-        	COUNT += 1
-        	if COUNT > COUNT1:
-            	COUNT1 = COUNT
-            	S = i+1
-    	else:
-        	COUNT = 0
-	STR1 = S - COUNT1
-	print(STR[STR1:S+1])
+    """This program is used to print longest alphabetical sequence to the given string"""
+    STR = input("enter string ")
+    S = 0
+    COUNT1 = 0
+    COUNT = 0
+    for i in range(len(STR)-1):
+        if STR[i] <= STR[i+1]:
+            COUNT += 1
+            if COUNT > COUNT1:
+                COUNT1 = COUNT
+                S = i+1
+        else:
+            COUNT = 0
+    STR1 = S - COUNT1
+    print(STR[STR1:S+1])
 if __name__ == "__main__":
-	main()
+    main()
