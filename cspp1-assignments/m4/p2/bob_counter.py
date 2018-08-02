f4bd3b5696baaff1cf54cb992ad361c6acfe9d34
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-02 19:36:32 +0530

    " -> 5 of 5 passed. pylint: 5.56/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -1,17 +1,17 @@
 '''Assume s is a string of lower case characters.
 
-Write a program that prints the number of times the string 'bob' occurs in s. 
+Write a program that prints the number of times the string 'bob' occurs in s.
 For example, if s = 'azcbobobegghakl', then your program should print
 
 Number of times bob occurs is: 2'''
 
 def main():
     ''' this program is used to print the bob count in a given string'''
-    STR_A = input()
-    COUNT_A = 0
-    for CHAR_A in range(len(STR_A) - 2):
-        if STR_A[CHAR_A] == 'b' and STR_A[CHAR_A+1] == 'o' and STR_A[CHAR_A+2] == 'b':
-            COUNT_A += 1
-    print(COUNT_A)
+    STR_VAL = input()
+    COUNT_VAL = 0
+    for CHAR_VAL in range(len(STR_VAL) - 2):
+        if STR_VAL[CHAR_VAL] == 'b' and STR_VAL[CHAR_VAL+1] == 'o' and STR_VAL[CHAR_VAL+2] == 'b':
+            COUNT_VAL += 1
+    print(COUNT_VAL)
 if __name__ == "__main__":
     main()
