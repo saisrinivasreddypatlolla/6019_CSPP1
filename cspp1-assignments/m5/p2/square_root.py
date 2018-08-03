e010ed184d3e901deac28813facb236ca32f7388
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-03 18:39:32 +0530

    " -> 0 of 1 passed. pylint: 8.75/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -2,21 +2,21 @@
 # using approximation method'''
 
 def main():
-	'''this program is used to check given number is perfect square or not'''
-	num_val = int(input(" enter number"))
-	guess = 0
-	epsilon = 0.1
-	inc = 0.1
-	num_a = 0
-	while guess < num_val:
-		if abs(guess**2-num_val) < epsilon:
-			break
-		guess = guess+inc
-		num_a += 1
-	if abs(guess**2-num_val) >= epsilon:
-		print(num_val, 'is not a perfect square')
-	else:
-		print(num_val, 'is a perfect square')
+    '''this program is used to check given number is perfect square or not'''
+    num_val = int(input(" enter number"))
+    guess = 0
+    epsilon = 0.1
+    inc = 0.1
+    num_a = 0
+    while guess < num_val:
+        if abs(guess**2-num_val) < epsilon:
+            break
+        guess = guess+inc
+        num_a += 1
+    if abs(guess**2-num_val) >= epsilon:
+        print(num_val, 'is not a perfect square')
+    else:
+        print(num_val, 'is a perfect square')
 
 if __name__ == "__main__":
-	main()
+    main()
