0126fdaf9c408e9825baedea132c6de80c805a8c
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-03 19:14:16 +0530

    " -> 3 of 3 passed. pylint: 9.29/10 "

--- cspp1-assignments/m5/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -11,13 +11,13 @@
 '''
 def main():
     ''' this program is used to print the squareroot value of the given number using bi-section method'''
-    s = int(input())
+    num_val = int(input())
     epsilon = 0.01
     low = 0
-    high = s
+    high = num_val
     avg = (low+high)/2
-    while abs(avg**2-s) >= epsilon:
-        if avg**2 < s:
+    while abs(avg**2-num_val) >= epsilon:
+        if avg**2 < num_val:
             low = avg
         else:
             high = avg
