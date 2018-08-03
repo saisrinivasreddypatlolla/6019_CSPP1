2ba0e8caf1ec30cdd4e01c84d867db67e50b677a
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-03 19:33:26 +0530

    " -> 3 of 3 passed. pylint: 8.89/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -10,13 +10,13 @@
 # output: 6.999999999999991
 '''
 def main():
-	''' print the given number is perfect squre or not using newtons rapson method'''
-	num_val = int(input())
-	guess = num_val/2.0
-	epsilon = 0.01
-	while (guess*guess-num_val) >= epsilon and guess <= num_val:
-		guess = guess-(((guess**2)-num_val)/(2*guess))
-	print(guess)
+    '''print the given number is perfect squre or not using newtons rapson method'''
+    num_val = int(input())
+    guess = num_val/2.0
+    epsilon = 0.01
+    while (guess*guess-num_val) >= epsilon and guess <= num_val:
+        guess = guess-(((guess**2)-num_val)/(2*guess))
+    print(guess)
 
 if __name__== "__main__":
-	main()
+    main()
