6aeb546a8e5d9efc6d06dd1820894e6d338d44e1
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-06 18:07:18 +0530

    " -> 10 of 10 passed. pylint: 9.38/10 "

--- cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -13,12 +13,9 @@ def paying_debt_off_in_a_year(balance, annual_interest_rate, monthly_payment_rat
         monthly_intrest_rate = (annual_interest_rate/12.0)
         minimum_monthly_payment = (monthly_payment_rate * previous_balance)
         monthly_unpaid_balance = previous_balance - minimum_monthly_payment
-        updated_balance_each_month = monthly_unpaid_balance + (monthly_intrest_rate*monthly_unpaid_balance)
+        updated_balance_each_month = monthly_unpaid_balance+(monthly_intrest_rate*monthly_unpaid_balance)
         previous_balance = updated_balance_each_month
     return round(updated_balance_each_month, 2)
-    
-
-
 def main():
     '''this program is used to print the remaining balance of credit card after one year'''
     data = input()
@@ -28,4 +25,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
