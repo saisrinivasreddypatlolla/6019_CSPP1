b299d89f2e8ea4c788c2193ab69f47c2401fcdba
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-07 19:48:33 +0530

    string in it

--- cspp1-practice/m8/IsInExercise/is_in.py
+++ cspp1-practice/m8/IsInExercise/is_in.py
@@ -3,20 +3,42 @@
 
 # This function takes in two arguments character and String and returns one boolean value.
 
-def isIn(char, aStr):
+def isIn(char, astr):
     '''
     char: a single character
     aStr: an alphabetized string
     
     returns: True if char is in aStr; False otherwise
     '''
-    # Your code here
+   
+    l = len(astr)   
+    #print("this is len",l)
+    if l == 0:
+        return False
+    if l == 1:
+        #print("le ==1  AND xhar is ",astr)
+        if char == astr[0]:
+            #print("char is :",char , "str is ",astr)
+            return True
+        else:
+            return False
+    else:
+        mid = len(astr)//2
+        #print(mid)
+        if char == astr[mid]:
+            return True
+        elif char < astr[mid]:
+            return isIn(char, astr[:mid])
+        else:
+            return isIn(char, astr[mid:])
+        
    
 
 def main():
     data = input()
     data = data.split()
-    print(isIn((data[0][0]), data[1]))
+    astr1 = sorted(data[1])
+    print(isIn((data[0][0]), astr1))
 
 
 if __name__== "__main__":
