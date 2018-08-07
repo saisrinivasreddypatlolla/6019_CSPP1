868cde418bd6841b094d5025522a85a799e96347
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-07 13:45:42 +0530

    " -> 9 of 10 passed. pylint: 6.36/10 "

--- cspp1-assignments/m8/p2/assignment2.py
+++ cspp1-assignments/m8/p2/assignment2.py
@@ -4,23 +4,22 @@ import sys
 
 # This function takes in one number and returns one number.
 sys.setrecursionlimit(15000)
-def sumofdigits(n):
+def sumofdigits(number):
     '''
     n is positive Integer
 
     returns: a positive integer, the sum of digits of n.
     '''
     # Your code here
-    if n <= 0:
-    	return 0
-    else:
-    	return n%10+sumofdigits(n//10)
+    if number <= 0:
+        return 0
+    return number%10+sumofdigits(number//10)
 
 
 def main():
-    a = input()
-    print(sumofdigits(int(a)))  
+    answer = input()
+    print(sumofdigits(int(answer)))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
 
