b6febd14c494b03404b8c18c55dce86d707362ed
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-11 12:06:06 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/create_social_network.py
@@ -39,21 +39,19 @@ def create_social_network(data):
     temp_list_two = []
     if '\n' not in data:
         return dictionary
-    else:
-        input_string = data.splitlines()
-        for followers in input_string:
-            temp_list_one.append(followers.split(" follows "))
-        for following in temp_list_one:
-            for index_of_follwing in range(len(following)):
-                temp_list_two.append(following[index_of_follwing].split(','))
-
-        for index_of_list in range(0, len(temp_list_two), 2):
-            key_in_dictionary = temp_list_two[index_of_list]
-            values_in_dictionaries = temp_list_two[index_of_list+1]
-            if key_in_dictionary[0] in dictionary.keys():
-                dictionary.append(key_in_dictionary, values_in_dictionaries)
-            else:
-                dictionary[key_in_dictionary[0]] = values_in_dictionaries
+     input_string = data.splitlines()
+    for followers in input_string:
+        temp_list_one.append(followers.split(" follows "))
+    for following in temp_list_one:
+        for index_of_follwing in enumerate(following):
+            temp_list_two.append(following[index_of_follwing].split(','))
+    for index_of_list in range(0, len(temp_list_two), 2):
+        key_in_dictionary = temp_list_two[index_of_list]
+        values_in_dictionaries = temp_list_two[index_of_list+1]
+        if key_in_dictionary[0] in dictionary.keys():
+            dictionary.append(key_in_dictionary, values_in_dictionaries)
+        else:
+          dictionary[key_in_dictionary[0]] = values_in_dictionaries
         return dictionary
 
 
