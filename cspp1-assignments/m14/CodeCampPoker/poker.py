fced9cbcf08ac8eb29674a9bae6422067d769800
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-16 11:25:36 +0530

    " -> 16 of 19 passed. pylint: 9.89/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -156,7 +156,7 @@ def is_high_card(hand):
         else:
             temporary_list_for_high_values.append(float(card_in_hand[0])/100)
         #print(temporary_list_for_high_values)
-    temporary_list_for_high_values = sorted(temporary_list_for_high_values)
+    #temporary_list_for_high_values = sorted(temporary_list_for_high_values)
     return max(temporary_list_for_high_values)
 def hand_rank(hand):
     '''
