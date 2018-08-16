e9bbc7d85c4f6befbd7be01966a209c5c609c165
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-16 11:17:52 +0530

    " -> 16 of 19 passed. pylint: 9.89/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -155,7 +155,7 @@ def is_high_card(hand):
             temporary_list_for_high_values.append(0.10)
         else:
             temporary_list_for_high_values.append(float(card_in_hand[0])/100)
-    return max(set(sorted(hand)))
+    return max(temporary_list_for_high_values)
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
