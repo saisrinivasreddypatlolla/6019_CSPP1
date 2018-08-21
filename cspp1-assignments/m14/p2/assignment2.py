e113b2a8e881ddcb7c7272ca902c59fda99b42b0
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-21 23:30:26 +0530

    " -> 5 of 5 passed. pylint: 8.96/10 "

--- cspp1-assignments/m14/p2/assignment2.py
+++ cspp1-assignments/m14/p2/assignment2.py
@@ -135,7 +135,7 @@ class Message(object):
 class PlaintextMessage(Message):
     def __init__(self, text, shift):
         '''
-        Initializes a PlaintextMessage object        
+        Initializes a PlaintextMessage object
         
         text (string): the message's text
         shift (integer): the shift associated with this message
@@ -147,7 +147,7 @@ class PlaintextMessage(Message):
             self.encrypting_dict (dictionary, built using shift)
             self.message_text_encrypted (string, created using shift)
 
-        Hint: consider using the parent class constructor so less 
+        Hint: consider using the parent class constructor so less
         code is repeated
         '''
         self.shift = shift
@@ -186,8 +186,8 @@ class PlaintextMessage(Message):
 
     def change_shift(self, shift):
         '''
-        Changes self.shift of the PlaintextMessage and updates other 
-        attributes determined by shift (ie. self.encrypting_dict and 
+        Changes self.shift of the PlaintextMessage and updates other
+        attributes determined by shift (ie. self.encrypting_dict and
         message_text_encrypted).
         
         shift (integer): the new shift that should be associated with this message.
