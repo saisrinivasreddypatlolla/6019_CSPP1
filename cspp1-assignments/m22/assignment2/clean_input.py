951f22c066c9df02680c34a252c7eeed74bbf120
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-25 10:52:57 +0530

    " -> 2 of 3 passed. pylint: 8.00/10 "

--- cspp1-assignments/m22/assignment2/clean_input.py
+++ cspp1-assignments/m22/assignment2/clean_input.py
@@ -4,7 +4,16 @@ alphabets in both upper and lower case and numbers.
 '''
 
 def clean_string(string):
-    pass
+    list1 = string.split()
+    characters = "!@#$%^&*()"
+    list2 = []
+    for word in list1:
+        #print(char)
+        for char in characters:
+            if char in word:
+                word = word.replace(char, '')
+        list2.append(word)
+    return ''.join(list2)
 
 def main():
     string = input()
