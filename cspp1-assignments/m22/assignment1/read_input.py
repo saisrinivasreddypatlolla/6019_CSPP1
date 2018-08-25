2dbb1c8510cbc73dc7a60e5a46319e889cfaa2b9
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-25 10:36:51 +0530

    " -> 2 of 2 passed. pylint: 4.29/10 "

---
+++ cspp1-assignments/m22/assignment1/read_input.py
@@ -0,0 +1,12 @@
+'''
+Write a python program to read multiple lines of text input and store the input into a string.
+'''
+
+def main():
+    number = int(input())
+    for line in range(number):
+    	string = input()
+    	print(string)
+
+if __name__ == '__main__':
+    main()
--- cspp1-practice/m15/Inheritance-Exercise on genPrimes/gen_primes.py
+++ cspp1-practice/m15/Inheritance-Exercise on genPrimes/gen_primes.py
@@ -1,17 +1,26 @@
 #define the gen_primes function here
 def genPrimes():
-    pass
+	number = 2
+	while True:
+		if is_prime(number):
+			yield number
+		number += 1
+def is_prime(number):
+	for value in range(2, number):
+		if number%value == 0:
+			return False
+	return True
 
 def main():
-	data=raw_input()
+	data=input()
 	l=data.split()
 	a=int(l[0])
 	b=int(l[1])
 	primeGenerator = genPrimes()
 	for i in range(a):
-	    p=primeGenerator.next()
+	    p=primeGenerator.__next__()
 	    if(i%b==0):
-	        print p
+	        print(p)
 	
 if __name__== "__main__":
 	main()
