6469ac851bc7d9a966a45c20774db029499067ef
saisrinivasreddypatlolla <nivasreddy08@msitprogram.net>
2018-08-25 12:31:04 +0530

    " -> 26 of 26 passed. pylint: 8.60/10 "

---
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -0,0 +1,79 @@
+'''
+    Sudoku is a logic-based, combinatorial number-placement puzzle.
+    The objective is to fill a 9×9 grid with digits so that
+    each column, each row, and each of the nine 3×3 subgrids that compose the grid
+    contains all of the digits from 1 to 9.
+
+    Complete the check_sudoku function to check if the given grid
+    satisfies all the sudoku rules given in the statement above.
+'''
+# def grid_check(sudoku, list1):
+#     list2 = []
+#     for row in range(3):
+#         for column in range(3):
+
+
+def column_check(sudoku, list1):
+    count = 0
+    length = len(sudoku)
+    list3 = []
+    for column in range(length):
+        list2 = []
+        for value in range(length):
+            list2.append(sudoku[value][column])
+        list3.append(list2)
+    for column in list3:
+        sort = sorted(column)
+        if list1 == sort:
+            count += 1
+        else:
+            return False
+    return count == len(sudoku)
+
+def row_check(sudoku, list1):
+    count = 0
+    for row in sudoku:
+        sort = sorted(row)
+        if list1 == sort:
+            count += 1
+        else:
+            return False
+    return count == len(sudoku)
+
+def check_sudoku(sudoku):
+    '''
+        Your solution goes here. You may add other helper functions as needed.
+        The function has to return True for a valid sudoku grid and false otherwise
+    '''
+    list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    if row_check(sudoku, list1) and column_check(sudoku, list1):# and grid_check(sudoku, list1):
+        return True
+    return False
+    count = 0
+    for row in sudoku:
+        sort = sorted(row)
+        if list1 == sort:
+            count += 1
+        else:
+            return False
+    return count == len(sudoku)
+
+def main():
+    '''
+        main function to read input sudoku from console
+        call check_sudoku function and print the result to console
+    '''
+    
+    # initialize empty list
+    sudoku = []
+
+    # loop to read 9 lines of input from console
+    for i in range(9):
+        # read a line, split it on SPACE and append row to list
+        row = list(map(int, input().split(' ')))
+        sudoku.append(row)
+    # call solution function and print result to console
+    print(check_sudoku(sudoku))
+
+if __name__ == '__main__':
+    main()
